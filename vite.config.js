255c5eb71c9fa45cc5d146cec69258dbfe773660
J Soren <45916175+j-soren@users.noreply.github.com>
2026-03-31 17:25:48 +0530

    Added vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // THIS MUST MATCH YOUR GITHUB REPOSITORY NAME EXACTLY!
+  // Example: If your repo is github.com/username/shilpayan-portfolio
+  // Then the base should be '/shilpayan-portfolio/'
+  base: '/shilpayan-portfolio/', 
+  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        about: resolve(__dirname, 'about.html')
+      }
+    }
+  }
+})
